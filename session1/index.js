cde1d74695d8856f2bf44bccca142836600459f9
hoangduc221997 <duongtom.221@gmail.com>
2020-04-28 22:49:55 +0700

    session1

---
+++ session1/index.js
@@ -0,0 +1,84 @@
+// let name = "Trung"
+
+// name = "Hùng"
+// name = 10
+
+//OUT
+// alert(`Xin chào ${name}`)
+// console.log("Xin chào", name)
+// crtl + /
+
+// IN
+
+// let name = prompt("What's your name")
+// alert(`Xin chào ${name}`)
+
+// alert(`Bạn có khỏe không ${name}`)
+// alert(`Bạn bao nhiêu tuổi ${name}`)
+
+// CamelStyle
+
+// let hoVaTen
+
+
+//Data Types
+
+
+// let n = 10
+
+//Number
+// +, - , * , /
+// alert(n*5)
+// %
+// alert(n%3)
+
+// +=, -=
+// n -= 10
+// alert(n)
+
+// ++, --
+// alert(n)
+// n--
+// alert(n)
+// n++
+// alert(n)
+
+// Làm tròn xuống
+// console.log(Math.floor(5.4))
+// // Làm tròn lên
+// console.log(Math.ceil(5.4))
+// //Trị tuyệt đối
+// console.log(Math.abs(-4))
+// // Căn bậc
+// console.log(Math.sqrt(4))
+
+//w3school
+
+//String
+let s = 'Phạm Đức Trung'
+let name = "hihi"
+//+
+// console.log(s + " Đẹp zai")
+// console.log(s + 10)
+
+// alert(name.toUpperCase())
+// alert(s.toLowerCase())
+
+let age = 30
+
+// console.log("Bạn này", age , "tuổi")
+console.log(`Bạn này ${age} tuổi`)
+
+// Data Type Conversion
+
+//String to Number
+
+let a = "50"
+console.log(Number(a))
+
+// Number to String
+let n = 100
+console.log(String(n))
+console.log(`Bạn này ${n} tuổi`)
+
+
---
+++ session1/pratice.js
@@ -0,0 +1,8 @@
+// Cho người dùng nhập vào số a và b. 
+// Tính tổng của a và b
+
+let a = Number(prompt("Nhập số a đi bạn êi"))
+let b = Number(prompt("Nhập số b đi bạn êi"))
+// a = Number(a)
+// b = Number(b)
+alert(`Tổng của 2 số là: ${a+b}`)
